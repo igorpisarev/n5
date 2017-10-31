bfedf28f2d2fe33110dbfec7a10850e52cd12102
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-10-31 14:21:02 -0400

    add serializable array data block

---
+++ src/main/java/org/janelia/saalfeldlab/n5/SerializableArrayDataBlock.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2017, Stephan Saalfeld
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.janelia.saalfeldlab.n5;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+public class SerializableArrayDataBlock<T extends Serializable> extends AbstractDataBlock<T[]> {
+
+	public SerializableArrayDataBlock(final int[] size, final long[] gridPosition, final T[] data) {
+
+		super(size, gridPosition, data);
+	}
+
+	@Override
+	public ByteBuffer toByteBuffer() {
+
+		try (final ByteArrayOutputStream b = new ByteArrayOutputStream()) {
+			try (final ObjectOutputStream o = new ObjectOutputStream(b)) {
+				o.writeObject(getData());
+			}
+			return ByteBuffer.wrap(b.toByteArray());
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void readData(final ByteBuffer buffer) {
+
+		try (final ByteArrayInputStream b = new ByteArrayInputStream(buffer.array())) {
+			try (final ObjectInputStream o = new ObjectInputStream(b)) {
+				final T[] readData = (T[])o.readObject();
+				System.arraycopy(readData, 0, data, 0, Math.max(readData.length, data.length));
+			}
+		} catch (final IOException | ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public int getNumElements() {
+
+		return data.length;
+	}
+}
